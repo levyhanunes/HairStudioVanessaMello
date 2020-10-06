29038d89edd80417584eb075d0866d56a35d3d34
levyhanunes <levyhanunes@gmail.com>
2020-10-06 14:56:14 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/empiricus/sizzllingbites/MainActivity.java
@@ -0,0 +1,14 @@
+package com.empiricus.sizzllingbites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
