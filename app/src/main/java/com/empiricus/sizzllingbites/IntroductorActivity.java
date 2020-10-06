c8351e24a18107243002e6fb9239de3fbb88c090
levyhanunes <levyhanunes@gmail.com>
2020-10-06 19:58:36 -0300

    implantei a biblioteca liquid-swipe e criei alguns fragments

--- app/src/main/java/com/empiricus/sizzllingbites/IntroductorActivity.java
+++ app/src/main/java/com/empiricus/sizzllingbites/IntroductorActivity.java
@@ -1,6 +1,11 @@
 package com.empiricus.sizzllingbites;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -12,6 +17,10 @@ public class IntroductorActivity extends AppCompatActivity {
     ImageView logo,appName,splashImg;
     LottieAnimationView lottieAnimationView;
 
+    private static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private ScreenSlidePagerAdapter pagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +31,45 @@ public class IntroductorActivity extends AppCompatActivity {
         splashImg =findViewById(R.id.img);
         lottieAnimationView =findViewById(R.id.lottie);
 
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+
+
         splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(2500);
         logo.animate().translationY(1400).setDuration(1000).setStartDelay(2500);
         appName.animate().translationY(1400).setDuration(1000).setStartDelay(2500);
         lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(2500);
 
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+
+        public ScreenSlidePagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    OnBoardingFragment1 tab1 = new OnBoardingFragment1();
+                return tab1;
+                case 1:
+                    OnBoardingFragment2 tab2 = new OnBoardingFragment2();
+                    return tab2;
+                case 2:
+                    OnBoardingFragment3 tab3 = new OnBoardingFragment3();
+                    return tab3;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
